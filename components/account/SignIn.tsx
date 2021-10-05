5c9a30cdd26827cadffa300a46231dbbf7cbed39
Aram Petrosyan <0aramp@gmail.com>
2021-10-05 19:33:39 +0400

    update sign in component

--- components/account/SignIn.tsx
+++ components/account/SignIn.tsx
@@ -19,8 +19,7 @@ export const SignIn: React.VFC = () => {
     handleSubmit,
     formState: { errors },
     setError,
-    setValue
-  } = useForm({
+  } = useForm<SignInFormData>({
     defaultValues: {
       email: "",
       password: "",
@@ -35,57 +34,57 @@ export const SignIn: React.VFC = () => {
     });
 
     if (data?.tokenCreate?.errors[0]) {
-      setError("password", { message: "Please, enter valid credentials" });
+      setError("email", { message: "Please, enter valid credentials" });
     }
   });
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center max-w-410">
       <div
         style={{ marginTop: 24 }}
         className="w-full flex justify-center items-center"
       >
         <Logo
-          className="ml-2"
+          className="ml-2 max-sm:w-38"
           containerClass="flex flex-col gap-x-1 items-center"
           link={false}
         >
-          <img src="/sit-stand-desk.svg" width="166px" alt="Sit Stand Desk" />
+          <img src="/sit-stand-desk.svg" width="166px" className="max-sm:w-130" alt="Sit Stand Desk" />
         </Logo>
       </div>
-      <p className="text-center text-black text-base px-5 mt-2 max-500:px-1">
+      <p className="text-center text-black text-base px-5 mt-2s max-sm:px-3 max-sm:text-xs max-sm:mt-2">
         {"Sign in to view all your profile and save products."}
       </p>
-      <form onSubmit={handleSignIn} className="w-full mt-6 text-gray-siv">
-        <div className="flex flex-col px-11 max-500:px-3">
+      <form onSubmit={handleSignIn} className="w-full mt-6 text-gray-siv max-sm:mt-3">
+        <div className="flex flex-col px-10 max-sm:px-7">
           <div className="w-full mt-5">
             <input
               type="text"
               id="email"
               autoComplete="email"
-              className="w-full border-gray-siv text-base focus:ring-0 focus:border-black text-black"
+              className="w-full border-gray-siv text-base active:border-black focus:ring-0 focus:border-black text-black py-7in max-sm:text-xs max-sm:py-3in"
               placeholder="Email address"
               {...register("email", {
                 required: "This field is required.",
               })}
             />
-            {!!errors.email && (
+            {errors.email && (
               <p className="text-red-blud text-xs">{errors.email.message}</p>
             )}
           </div>
 
-          <div className="w-full mt-5">
+          <div className="w-full mt-5 max-sm:mt-4">
             <input
               type="password"
               id="password"
               autoComplete="current-password"
-              className="w-full border-gray-siv text-base focus:ring-0 focus:border-black text-black"
+              className="w-full border-gray-siv text-base active:border-black focus:ring-0 focus:border-black text-black py-7in max-sm:text-xs max-sm:py-3in"
               placeholder="Password"
               {...register("password", {
                 required: "This field is required.",
               })}
             />
-            {!!errors.password && (
+            {errors.password && (
               <p className="text-red-blud text-xs">{errors.password.message}</p>
             )}
           </div>
@@ -93,33 +92,32 @@ export const SignIn: React.VFC = () => {
           <div className="flex items-center justify-between mt-3">
             <div className="flex items-center">
               <input
-                className="border-gray-siv focus:ring-0 focus:border-black cursor-pointer text-black"
+                className="border-gray-siv active:border-black focus:ring-0 focus:border-black cursor-pointer text-black max-sm:w-3.5 max-sm:h-3.5"
                 type="checkbox"
                 id="keepIn"
                 {...register("keepIn")}
               />
-              <p className="text-xs font-normal" style={{ marginLeft: 6 }}>
+              <p className="text-xs font-normal max-sm:text-9xs" style={{ marginLeft: 6 }}>
                 {"Keep me signed in"}
               </p>
             </div>
             <div>
-              <p className="text-xs font-normal cursor-pointer">
+              <p className="text-xs font-normal cursor-pointer max-sm:text-9xs">
                 {"Forgot Password?"}
               </p>
             </div>
           </div>
 
           <div
-            style={{ marginBottom: 30 }}
-            className="flex items-center flex-col gap-y-3 mt-9"
+            className="flex items-center flex-col gap-y-3 mt-9 mb-30 max-sm:mb-20 max-sm:mt-8"
           >
             <Button
-              className="w-full max-w-171 border-none border-0 bg-black"
-              onClick={() => {}}
+              className="w-full max-w-171 border-none border-0 bg-black max-sm:text-xs max-sm:w-134"
+              onClick={() => { }}
             >
               {"Sign In"}
             </Button>
-            <span className="text-base">
+            <span className="text-base max-sm:text-xs">
               {"Don’t have an account? "}
               <span
                 onClick={() => setModalType("signUp")}
